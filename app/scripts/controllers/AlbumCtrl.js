973ed2fabca00213df5a2813f8447445542fb910
jbag123 <joshcone@googlemail.com>
2017-08-17 19:55:55 +0100

    commiting assignment 5

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,7 @@
 (function() {
         function AlbumCtrl() {
                 // create an empty array
-                this.albumData = [];
-                // loop 12 times
-                for (var i = 0; i < 12; i++) {
-                        // push copies of albumPicasso to the empty array
-                        this.albumData.push(angular.copy(albumPicasso));
-                }
+                this.albumData = albumPicasso;
         }
         angular
                 .module('blocJams')
