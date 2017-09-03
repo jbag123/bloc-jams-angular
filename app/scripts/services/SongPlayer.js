165a9db3006764e43bee3cc748deef3d4b2da765
jbag123 <joshcone@googlemail.com>
2017-09-03 14:25:12 +0100

    commiting checkpoint 7 services part 2

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,55 @@
+(function() {
+        function SongPlayer() {
+                var SongPlayer = {};
+
+                var currentSong = null;
+                var currentBuzzObject = null;
+                /**
+                * @function setSong
+                * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+                * @param {Object} song
+                */
+                var setSong = function(song) {
+                    if (currentBuzzObject) {
+                        currentBuzzObject.stop();
+                        currentSong.playing = null;
+                    }
+                    /**
+                    * @desc Buzz object audio file
+                    * @type {Object}
+                    */
+                    currentBuzzObject = new buzz.sound(song.audioUrl, {
+                        formats: ['mp3'],
+                        preload: true
+                    });
+
+                    currentSong = song;
+                };
+
+                SongPlayer.play = function(song) {
+                        // if the currently playing song is not the one clicked on then stop the current song
+                        if (currentSong !== song) {
+
+                                currentBuzzObject.play();
+                                song.playing = true;
+
+                        // if currently playing song is the one clicked on then stop play the song
+                        } else if (currentSong === song) {
+                                if (currentBuzzObject.isPaused()) {
+                                        currentBuzzObject.play();
+                                }
+                        }
+                };
+
+                SongPlayer.pause = function(song) {
+                        currentBuzzObject.pause();
+                        song.playing = false;
+                };
+
+                return SongPlayer;
+        }
+
+        angular
+            .module('blocJams')
+            .factory('SongPlayer', SongPlayer);
+    })();
